c64f439cbbdbfd4dfb31dd68939e05ce46937400
narayan jha <narayan@opoyi.com>
2022-12-27 17:52:09 +0530

    mui config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { StyledEngineProvider } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyledEngineProvider injectFirst>
+      <Component {...pageProps} />
+    </StyledEngineProvider>
+  );
 }
